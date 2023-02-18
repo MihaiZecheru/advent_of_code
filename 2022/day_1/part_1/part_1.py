817468ec61b9005f2591d824452830c984d5a158
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-02-18 14:41:43 -0800

    Reformat for importing

---
+++ 2022/day_1/part_1/part_1.py
@@ -0,0 +1,30 @@
+import os
+
+def get_data(path="input.txt"):
+	with open(os.path.join(os.getcwd(), path)) as f:
+		return f.readlines()
+
+def format_data(data):
+	elves = []
+	elf = []
+
+	for line in data:
+		if line == '\n':
+			elves.append(elf)
+			elf = []
+		else: elf.append(int(line))
+
+	return elves
+
+def count_calories(elves):
+	return [sum(elf) for elf in elves]
+
+def main():
+	data = get_data()
+	elves = format_data(data)
+	elf_calories = count_calories(elves)
+	top_elf = max(elf_calories)
+	print(top_elf)
+
+if __name__ == "__main__":
+	main()
