84a44ed07500b674d08652f072abf275286e14dd
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-02-18 15:32:38 -0800

    Day 2 part 2

--- 2022/day_2/part_2/part_2.py
+++ 2022/day_2/part_2/part_2.py
@@ -1,8 +1,24 @@
 import sys
 sys.path.append("../part_1/")
+from part_1 import get_data
+
+# 3 = scissors, 2 = paper, 1 = rock
+# x = lose, y = tie, z = win
+# find what is needed to get the result designated by X, Y, or Z
+results = {
+  "AX": 3 + 0, "AY": 1 + 3, "AZ": 2 + 6,
+  "BX": 1 + 0, "BY": 2 + 3, "BZ": 3 + 6,
+  "CX": 2 + 0, "CY": 3 + 3, "CZ": 1 + 6,
+}
+
+def calculate_score(games):
+  return sum(results[game] for game in games)
 
 def main():
-  pass
+  games = get_data("../part_1/input.txt")
+  score = calculate_score(games)
+  print(score)
+  
 
 if __name__ == '__main__':
   main()
