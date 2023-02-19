2a84cedefe927acb43fce95244fcf5fc44a435b3
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-02-19 11:50:07 -0800

    Add basic structure day 7 part 1

--- 2022/day_7/part_1/part_1.py
+++ 2022/day_7/part_1/part_1.py
@@ -1,11 +1,43 @@
 import os
 
+class File(object):
+  name: str
+  parent: any # Dir object
+  size: int
+
+  def __init__(self, name, size, parent=None):
+    self.name = name
+    self.parent = parent
+    self.size = size
+
+  def __repr__(self):
+    return f"{self.name}: {self.size}"
+
+class Dir(object):
+  name: str
+  parent: any # Dir object
+  children: list # Dir objects
+  size: int
+
+  def __init__(self, name, parent=None):
+    self.name = name
+    self.parent = parent
+    self.children = []
+    self.size = 0
+
+  def __repr__(self):
+    return f"{self.name}: {self.size}"
+
+  def add_child(self, child: File or any): # File or Dir
+    self.children.append(child)
+    self.size += child.size
+
 def get_data(path="input.txt"):
   with open(os.path.join(os.getcwd(), path)) as f:
     return f.readlines()
 
 def main():
-  pass
+  data = get_data()
 
 if __name__ == '__main__':
   main()
