5b9770e41203e3e874ffee633b487bd18512a630
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-02-18 18:40:48 -0800

    Day 4 part 2

--- 2022/day_4/part_2/part_2.py
+++ 2022/day_4/part_2/part_2.py
@@ -2,8 +2,20 @@ import sys
 sys.path.append("../part_1/")
 from part_1 import get_data
 
+def get_all_cleanup_sites(start: int, end: int):
+  return range(start, end + 1)
+
+def find_overlapping_elf_pairs(elf_pairs):
+  for [elf1, elf2] in elf_pairs:
+    elf1_sites = set(get_all_cleanup_sites(elf1.start, elf1.end))
+    elf2_sites = set(get_all_cleanup_sites(elf2.start, elf2.end))
+    if elf1_sites & elf2_sites: # same as elf1_sites.intersection(elf2_sites)
+      yield [elf1, elf2]
+
 def main():
-  pass
+  elf_pairs = get_data("../part_1/input.txt")
+  find_overlapping_elf_pairs_count = len(list(find_overlapping_elf_pairs(elf_pairs)))
+  print(find_overlapping_elf_pairs_count)
 
 if __name__ == '__main__':
   main()
