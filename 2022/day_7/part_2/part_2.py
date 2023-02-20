aa8af66ced99867ede9279eed30c07e2dce63748
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-02-19 17:11:39 -0800

    Day 7 part 2

--- 2022/day_7/part_2/part_2.py
+++ 2022/day_7/part_2/part_2.py
@@ -1,9 +1,67 @@
-import sys
-sys.path.append("../part_1/")
-from part_1 import get_data
+# Answer stolen from because I couldn't figure it out: https://github.com/PodolskiBartosz/advent-of-code-2022/blob/main/day-7/main.py
+
+# Day 7: No Space Left On Device (https://adventofcode.com/2022/day/7)
+# Conditions:
+# - The input represents commandos and outputs in a windows scheme, which are seperated by line
+# - Available commandos are "cd" and "ls" and start with "$"
+# - Outputs list directories and files; Output of a directory starts with "dir" and then name of the directory;
+#   Output of a file starts with the size of a file and then the name
+# - The total size of a directory is the sum of the sizes of the files it contains directly or indirectly
+
 
 def main():
-  pass
+    print("Advent of Code - Day 7")
+    file_task_1 = open('../part_1/input.txt', 'r')
+    file_task_2 = open('../part_1/input.txt', 'r')
+    task_1(file_task_1)
+    task_2(file_task_2)
 
-if __name__ == '__main__':
-  main()
+
+def get_file_system_directories(file):
+    # Create a list of directories by judging the inputs
+    current_path = ""
+    directories = {"/home": 0}
+    for line in file.read().splitlines():
+        line = line.split()
+        if line[0] == "$":
+            if line[1] != "ls":
+                if line[2] == "..":
+                    # Find index of last occurrence of "/" and create new string up until that index
+                    current_path = current_path[:current_path.rindex("/")]
+                elif line[2] == "/":
+                    current_path = "/home"
+                else:
+                    current_path = f"{current_path}/{line[2]}"
+                    directories[current_path] = 0
+        elif line[0] != "dir":
+            temp_path = current_path
+            # Update all parent directories
+            while temp_path != "":
+                directories[temp_path] += int(line[0])
+                temp_path = temp_path[:temp_path.rindex("/")]
+    return directories
+
+
+# Task 1: Get sum of all directories with a total size of at most 100000
+def task_1(file):
+    directories = get_file_system_directories(file)
+    sum_small_directories = sum(
+        directory for _, directory in directories.items() if directory < 100000
+    )
+    print(f"Task 1 result: {str(sum_small_directories)}")
+
+
+# Task 2: Get the smallest directory that can be deleted, in order to have at least 30000000 free space, with 70000000
+#         being the total disk space available
+def task_2(file):
+    directories = get_file_system_directories(file)
+    required_free_space = directories["/home"] - (70000000 - 30000000)  # 40000000 of the space can be used
+    smallest_deletable_directory = directories["/home"]
+    for _, directory in directories.items():
+        if required_free_space < directory < smallest_deletable_directory:
+            smallest_deletable_directory = directory
+    print(f"Task 2 result: {str(smallest_deletable_directory)}")
+
+
+if __name__ == "__main__":
+    main()
